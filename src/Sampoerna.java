77686138abea9dbd9faf310c5c4556bf24abacfc
2much2write <hafidhaw@gmail.com>
2024-03-20 16:04:51 +0700

    crushing some bugs

--- src/Sampoerna.java
+++ src/Sampoerna.java
@@ -1,8 +1,6 @@
 public class Sampoerna {
     public static void main(String[] args) {
         int b=23;
-        int c=24;
-        System.out.println(c);
         System.out.println(b);
     }
 }
